5ddce695b5232d824ef03b0d45940e122ac7877e
pengwei <1119849897@qq.com>
2016-05-03 10:46:40 +0800

    init

--- example/app.js
+++ example/app.js
@@ -27,15 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 //ueditor访问的地址
 app.use("/ueditor/ue", nuq.ueditor(path.join(__dirname, 'public'), function(req, res, next) {
 
-    // ueditor 客户发起上传图片请求
+    // ueditor 客户发起上传请求
 
     if(req.query.action.indexOf('upload')===0){
         console.log(req.query.action)
         res.ue_up();
     }
-    //  客户端发起图片列表请求
+    //  客户端发起列表请求
     else if (req.query.action.indexOf('list')===0){
-        res.ue_list(req.query.action) // 客户端会列出 action 下的所有图片
+        res.ue_list(req.query.action) // 客户端会列出所有文件
     }
     // 客户端发起其它请求
     else {
